16873bf3eee567f9bac9de64b19f6440be688a97
SmbPndk <89791739+smbpndk@users.noreply.github.com>
2023-03-23 21:58:22 +0100

    Update cli.rb

--- Formula/cli.rb
+++ Formula/cli.rb
@@ -2,7 +2,7 @@ class Cli < Formula
   desc "CLI interface to access SmbPndk."
   homepage "https://github.com/smbpndk/smbpndk-cli"
   url "https://github.com/smbpndk/smbpndk-cli/releases/download/v0.2.0/smb-0.2.0-x86_64-apple-darwin.tar.gz"
-  sha256 "623788f5ba8ee77c06d577de4bbc74c667de6a1a"
+  sha256 "42e8dc21ae191b4f784dc7efae136f426680be3a1fa7d0af357dffe9d657597b"
   version "0.2.0"
 
   def install
