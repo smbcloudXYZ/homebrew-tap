3fa7bd93323e0316957ee5c4294572690bf94b0b
Seto <setoelkahfi@gmail.com>
2025-09-17 15:43:37 +0200

    v0.3.26

--- Formula/cli.rb
+++ Formula/cli.rb
@@ -1,9 +1,9 @@
 class Cli < Formula
-  desc 'CLI interface to access smbCloud.'
+  desc 'smbCloud command line interface.'
   homepage 'https://github.com/smbcloudXYZ/smbcloud-cli'
-  url 'https://github.com/smbcloudXYZ/smbcloud-cli/releases/download/v0.2.0/smb-0.2.0-x86_64-apple-darwin.tar.gz'
-  sha256 '623788f5ba8ee77c06d577de4bbc74c667de6a1a'
-  version '0.2.0'
+  url 'https://github.com/smbcloudXYZ/smbcloud-cli/releases/download/v0.3.26/smb-macos-arm64'
+  sha256 '79456ac0641433ffa4e1b1517192afe151888ec5'
+  version '0.3.26'
 
   def install
     bin.install 'smb'
